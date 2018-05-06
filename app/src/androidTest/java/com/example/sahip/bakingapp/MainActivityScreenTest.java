5a18dbd3291772c7a6f94a9ee046cd8b5120aa69
didem <kucukkaraaslan.didem@gmail.com>
2018-05-06 21:36:54 +0300

    MainActivityScreenTest

--- app/src/androidTest/java/com/example/sahip/bakingapp/MainActivityScreenTest.java
+++ app/src/androidTest/java/com/example/sahip/bakingapp/MainActivityScreenTest.java
@@ -2,10 +2,16 @@ package com.example.sahip.bakingapp;
 
 import android.support.test.espresso.Espresso;
 import android.support.test.espresso.IdlingResource;
+import android.support.test.espresso.ViewInteraction;
 import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.espresso.matcher.BoundedMatcher;
+import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+import android.support.v7.widget.Toolbar;
 
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
@@ -16,12 +22,17 @@ import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.scrollTo;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isAssignableFrom;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.is;
 
 @RunWith(AndroidJUnit4.class)
+@LargeTest
 public class MainActivityScreenTest {
-    public static final String RECIPE_NAME = "Nutella";
+    public static final String RECIPE_NAME = "Nutella Pie";
 
     @Rule
     public ActivityTestRule<MainActivity> mainActivityActivityTestRule =
@@ -45,6 +56,10 @@ public class MainActivityScreenTest {
                 RecyclerViewActions.actionOnItemAtPosition(0, click())
         );
 
+        // Check if the toolbar title matches with the recipe name
+        matchToolbarTitle(RECIPE_NAME)
+                .check(matches(isDisplayed()));
+
 
     }
 
@@ -53,6 +68,26 @@ public class MainActivityScreenTest {
         if (mIdlingResource != null) {
             Espresso.unregisterIdlingResources(mIdlingResource);
         }
+    }
+
+
+    private static ViewInteraction matchToolbarTitle(
+            CharSequence title) {
+        return onView(isAssignableFrom(Toolbar.class))
+                .check(matches(withToolbarTitle(is(title))));
+    }
+
+    private static Matcher<Object> withToolbarTitle(
+            final Matcher<CharSequence> textMatcher) {
+        return new BoundedMatcher<Object, Toolbar>(Toolbar.class) {
+            @Override public boolean matchesSafely(Toolbar toolbar) {
+                return textMatcher.matches(toolbar.getTitle());
+            }
+            @Override public void describeTo(Description description) {
+                description.appendText("with toolbar title: ");
+                textMatcher.describeTo(description);
+            }
+        };
     }
 
 }
